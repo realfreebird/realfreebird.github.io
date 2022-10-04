7b280c50faa3b8c94e056edcfcdd402ae5b6f8db
dror <drorpom@gmail.com>
2022-10-04 18:55:57 +0300

    isSelected & isSolved added to Cell

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,14 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+interface Word {
+  eng: string,
+  heb: string,
+  found: boolean,
+}
+
 class Cell {
 
   get letter() { return this._letter; }
   set letter(v) {
-    if (this._letter) {
-      throw new Error(`cell ${this.r}.${this.c} alreay set to '${this._letter}'`)
-    }
+    if (this._letter) { throw new Error(`cell ${this.r}.${this.c} alreay set to '${this._letter}'`); }
     this._letter = v;
   }
+
+  isSelected = false;
+  isSolved = false;
 
   constructor(private _letter: string, public r: number, public c: number, public _state_TODO?: any) { }
 }
@@ -17,6 +24,12 @@ function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
 
+
+interface RowCellSelection {
+  row: number;
+  startCol: number;
+  endCol: number;
+}
 
 @Component({
   selector: 'app-board',
@@ -27,13 +40,21 @@ export class BoardComponent implements OnInit {
 
   @Input() rows = 8;
   @Input() cols = 8;
-  @Input() words = ['cat', 'dog', 'wolf', 'fox', 'fish', 'monkey', 'pig'];
+  @Input() words: Word[] = [
+    { eng: 'cat', heb: 'חתול' },
+    { eng: 'dog', heb: 'כלב' },
+    { eng: 'wolf', heb: 'זאב' },
+    { eng: 'fox', heb: 'שועל' },
+    { eng: 'fish', heb: 'דג' },
+    { eng: 'monkey', heb: 'קוף' },
+    { eng: 'pig', heb: 'חזיר' }
+  ].map(x => ({ ...x, found: false }));
   // @Input() words = ['cat'];
   // @Input() words = ['12345678901']; // todo - throw error -or- ignore  when word is larger than board 
 
   cells: Array<Array<Cell>> = []
 
-  constructor() { }
+  constructor() { (window as any).board = this; }
 
   ngOnInit(): void {
     this.init()
@@ -50,7 +71,7 @@ export class BoardComponent implements OnInit {
       const row = this.cells[r];
       for (let c = 0; c < this.cols; c++) {
         if (row[c].letter) { continue; }
-        const l = String.fromCharCode(97+getRandomInt(26))
+        const l = String.fromCharCode(97 + getRandomInt(26))
         row[c].letter = l
       }
     }
@@ -87,9 +108,9 @@ export class BoardComponent implements OnInit {
       return null;
     }
 
-    const errors: string[] = [];
+    const errors: Word[] = [];
     for (const w of this.words) {
-      let cell0 = findRandomEmptyCell(w.length);
+      let cell0 = findRandomEmptyCell(w.eng.length);
       if (!cell0) {
         errors.push(w);
         continue;
@@ -97,7 +118,7 @@ export class BoardComponent implements OnInit {
 
       let cell = cell0;
 
-      for (const letter of w) {
+      for (const letter of w.eng) {
         cell.letter = letter;
         const nextCell = this.cells[cell.r][cell.c + 1];
         cell = nextCell;
@@ -109,5 +130,68 @@ export class BoardComponent implements OnInit {
       alert('could not fit all words: ' + JSON.stringify(errors))
     }
   }
+
+  getSelectionRange(cell: Cell): RowCellSelection {
+
+    const row = this.cells[cell.r];
+    const col = cell.c;
+
+    let from = col, to = col;
+    for (; to < row.length - 1; to++) {
+      if (!row[to + 1].isSelected) { break; }
+    }
+    for (; from > 0; from--) {
+      if (!row[from - 1].isSelected) { break; }
+    }
+
+    return {
+      row: cell.r,
+      startCol: from,
+      endCol: to
+    }
+  }
+
+  // selCount = 0;
+
+  getWordFromselection(cells: RowCellSelection) {
+    const row = this.cells[cells.row];
+    let word = ''
+    for (let i = cells.startCol; i <= cells.endCol; i++) {
+      word += row[i].letter;
+    }
+    return word;
+  }
+
+  markSolved(cells: RowCellSelection) {
+    const row = this.cells[cells.row];
+    for (let i = cells.startCol; i <= cells.endCol; i++) {
+      const c = row[i];
+      c.isSelected = false;
+      c.isSolved = true;
+    }
+  }
+
+  toggleCellSelection(c: Cell) {
+    if (c.isSolved) { return; /* nothing todo */ }
+
+    c.isSelected = !c.isSelected;
+    if (!c.isSelected) { return; }
+
+    // debugger;
+    const selCell = this.getSelectionRange(c);
+    if (!selCell) { return; }
+    const selWord = this.getWordFromselection(selCell);
+    if (selWord?.length <= 1) { return; }
+
+    setTimeout(() => {
+
+      const word = this.words.find(w => w.eng === selWord);
+      if (word) {
+        this.markSolved(selCell);
+        word.found = true;
+      }
+    }, 0);
+  }
+
 
 }
